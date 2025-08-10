1d2985f89c12b7eb47414170f9fcee8bef89d65e
sa7iiii7 <159489630+sa7iiii7@users.noreply.github.com>
2025-08-10 18:13:05 -0400

    Add .next/static/chunks/src_app_page_tsx_269f1c1c._.js 1754863984887

---
+++ .next/static/chunks/src_app_page_tsx_269f1c1c._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_34389d2b._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_ca1a64df._.js",
+  "static/chunks/node_modules_@radix-ui_020e7188._.js",
+  "static/chunks/node_modules_@floating-ui_9ec1fa39._.js",
+  "static/chunks/node_modules_03f77b4d._.js"
+],
+    source: "dynamic"
+});
